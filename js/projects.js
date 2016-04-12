373ffc445d6d066f7890d987afa42ac70189c1a8
dstineback <dstineback@gmail.com>
2016-04-11 21:21:56 -0700

    added a object construction function

---
+++ js/projects.js
@@ -0,0 +1,9 @@
+var projects = [];
+
+function Projects(opts){
+  this.title = opts.ttile;
+  this.developer = opts.developer;
+  this.date = opts.date;
+  this.projectURL = opts.projectURL;
+  this.projectDescription = opts.projectDescription;
+}
