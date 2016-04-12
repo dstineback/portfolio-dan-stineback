8c8ace330eb99c76b5861105a65bd5e0d3521c5e
dstineback <dstineback@gmail.com>
2016-04-11 21:04:16 -0700

    added data to about the projects

--- js/projectData.js
+++ js/projectData.js
@@ -1,5 +1,24 @@
 var projectData = [
+  {
+    title: 'Budget Buddy',
+    developer:  'Dan Stineback',
+    date: '2016-04-',
+    projectURL: 'https://github.com/dstineback/budget-buddy',
+    projectDescription: 'Final group project for 201...more info to come.'
+  },
+  {
+    title: 'Bus Mall',
+    developer:  'Dan Stineback',
+    date: '2016-03-29',
+    projectURL: 'https://github.com/dstineback/bus-mall/tree/friday',
+    projectDescription: 'Class project for a bus app company...more info to come'
+  },
+  {
+    title: 'About Me',
+    developer:  'Dan Stineback',
+    date: '2016-03-16',
+    projectURL: 'https://github.com/dstineback/about-me-1/tree/master',
+    projectDescription: 'Class project about me...more info to come'
+  },
 
-
-  
 ];
