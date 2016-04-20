eed029919183418ec9681b591e633343300b7f5f
dstineback <dstineback@gmail.com>
2016-04-20 15:31:34 -0700

    took out not needed array on JSON page

--- scripts/template.js
+++ scripts/template.js
@@ -1,52 +1,51 @@
-
 (function(module){
 
-function Project(opts){
-  for (key in opts) this[key] = opts[key];
-};
+  function Project(opts){
+    for (key in opts) this[key] = opts[key];
+  };
 
-Project.prototype.toMyPortfolio = function(){
-  var template = Handlebars.compile($('#projects-template').html());
-  this.daysAgo = parseInt((new Date() - new Date(this.date)) / 60 / 60 / 24 / 1000);
-  return template(this);
-};
+  Project.prototype.toMyPortfolio = function(){
+    var template = Handlebars.compile($('#projects-template').html());
+    this.daysAgo = parseInt((new Date() - new Date(this.date)) / 60 / 60 / 24 / 1000);
+    return template(this);
+  };
 
 
-Project.all = [];
+  Project.all = [];
 
-Project.loadAll = function(dataFromJSON){
-  dataFromJSON.sort(function(a,b){
-    return (new Date(b.date)) - (new Date(a.date));
-  });
-  dataFromJSON.forEach(function(ele){
-    Project.all.push(new Project(ele));
-  });
-};
+  Project.loadAll = function(dataFromJSON){
+    dataFromJSON.sort(function(a,b){
+      return (new Date(b.date)) - (new Date(a.date));
+    });
+    dataFromJSON.forEach(function(ele){
+      Project.all.push(new Project(ele));
+    });
+  };
 
-Project.fetchAll = function(callback){
-  $.ajax({
-    type: 'HEAD',
-    url: 'scripts/projectData.json',
-    success: function(data, message, xhr){
-      var eTag = xhr.getResponseHeader('eTag');
-      if (eTag === localStorage.eTag){
-        Project.loadAll(JSON.parse(localStorage.stashedProjectData));
-        // projectView.initIndexPage();
-        callback();
-      } else {
-        $.getJSON('scripts/projectData.json', function(data){
-          Project.loadAll(data);
-          localStorage.stashedProjectData = JSON.stringify(data);
-          localStorage.eTag = eTag;
+  Project.fetchAll = function(callback){
+    $.ajax({
+      type: 'HEAD',
+      url: 'scripts/projectData.json',
+      success: function(data, message, xhr){
+        var eTag = xhr.getResponseHeader('eTag');
+        if (eTag === localStorage.eTag){
+          Project.loadAll(JSON.parse(localStorage.stashedProjectData));
           // projectView.initIndexPage();
           callback();
-        });
+        } else {
+          $.getJSON('scripts/projectData.json', function(data){
+            Project.loadAll(data);
+            localStorage.stashedProjectData = JSON.stringify(data);
+            localStorage.eTag = eTag;
+            // projectView.initIndexPage();
+            callback();
+          });
+        }
       }
-    }
-  });
-};
+    });
+  };
 
-Project.numWordsAll = function() {
+  Project.numWordsAll = function() {
 
     return Project.all.map(function(projects) {
       return projects.body.match(/\b\w+/g).length;
@@ -89,5 +88,5 @@ Project.numWordsAll = function() {
     });
   };
 
-module.Project = Project;
+  module.Project = Project;
 })(window);
